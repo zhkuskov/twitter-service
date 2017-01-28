d6f05c8167b7c905298636441a7390d048cf4d39
Евгений <me.kuskov@gmail.com>
2017-01-28 10:05:57 +0300

    Add files via upload

--- src/android/TwitterExampleActivity.java
+++ src/android/TwitterExampleActivity.java
@@ -3,11 +3,14 @@ package com.red_folder.twitter;
 import org.apache.cordova.*;
 import android.os.Bundle;
 
-public class TwitterExampleActivity extends DroidGap {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
-    }
-}
+
+
+public class TwitterExampleActivity extends Activity implements CordovaInterface { 
+CordovaWebView cwv; 
+/* Called when the activity is first created. */
+
+ @Override public void onCreate(Bundle savedInstanceState) { 
+super.onCreate(savedInstanceState);
+ /*setContentView(R.layout.main); 
+cwv = (CordovaWebView) findViewById(R.id.tutorialView);*/ Config.init(this); cwv.loadUrl(Config.getStartUrl());
+ }
