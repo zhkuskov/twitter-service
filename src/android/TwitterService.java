67ae7d306a0b654df8bfa62e9e9f0ce99a2ed093
Евгений <me.kuskov@gmail.com>
2017-01-28 03:04:30 +0300

    Add files via upload

--- src/android/TwitterService.java
+++ src/android/TwitterService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+import  	java.net.URL;
 import  	java.net.URLConnection;
 import  	java.net.HttpURLConnection;
 /*import org.apache.http.HttpResponse;
